3a9fecfd942a329271e24504517d86efeac9c169
Ankit Dhakad <231220011@nitdelhi.ac.in>
2026-01-19 19:42:33 +0530

    first commit

---
+++ Backend/src/app.js
@@ -0,0 +1,49 @@
+
+import dotenv  from "dotenv";
+
+dotenv.config()
+
+import express, { urlencoded } from "express";
+import cors from "cors"
+import router from "./Routes/Profile.routes.js";
+
+
+const app=express()
+
+app.use(express.urlencoded({extended:true}))
+
+app.use(express.json())
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://predusk-ka-assingment.vercel.app"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (Postman, curl)
+    if (!origin) return callback(null, true);
+
+    console.log(origin)
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
+app.get('/check', (req, res) => {
+  return res.status(200).json({
+    message: "Everything is working here"
+  });
+});
+
+
+//calling the routes
+app.use('/api/v1',router)
+
+
+export default app
+
